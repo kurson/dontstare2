f6645e3fdb15fd9af58542e3d7df678cfa1733fb
mussorgsky <mateusz.gorzynski@gmail.com>
2015-09-23 10:37:15 +0200

    Update main.cpp

--- dontstare2/main.cpp
+++ dontstare2/main.cpp
@@ -5,14 +5,17 @@
 
 
 int main() {
-	Screen mainscreen(vec2(80, 24));
-	mainscreen.addBox(Renderbox(vec2(0, 0), vec2(80, 24), false, true, ' '));
+	Screen mainscreen(vec2(80, 25));
+	mainscreen.addBox(Renderbox(vec2(0, 1), vec2(20, 10), true, true, ' '));
+	mainscreen.addBox(Renderbox(vec2(40, 1), vec2(40, 15), false, true, ' '));
+	mainscreen.getBox(0)->writeLine("HALO HALO, WITAM TU $CORPIONS, PODAJEMY KOMUNIKAT, YEBAC YETI, DZIEKUJE ZA UWAGE, DOBRANOC", vec2(0, 0));
+	mainscreen.getBox(1)->writeLine("Kurde bele czy to dziala czy nie dziala zaraz sie dowiemy", vec2(0, 0));
+	mainscreen.addBox(Renderbox(vec2(25, 11), vec2(30, 3), true, true, ' '));
+	mainscreen.getBox(2)->writeLine("Czy chcesz kontynuowac? Y/N", vec2(1, 1));
 	
-	Player bob(vec2(0, 0));
-
 	while (true) {
-		bob.changePosition(vec2(2, 1));
-		std::cout << bob.getPosition().x << " "<< bob.getPosition().y << std::endl;
-		//mainscreen.update();
+		mainscreen.update();
+		Renderbox* box = mainscreen.getBox(0);
+		box->setBorder(!box->getBorder());
 	}
 }
