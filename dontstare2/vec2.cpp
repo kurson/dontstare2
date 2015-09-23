ca0068a7cd577cf3012ac2eaef389f44c85d41d3
mussorgsky <mateusz.gorzynski@gmail.com>
2015-09-23 10:32:05 +0200

    Overload + - * /

--- dontstare2/vec2.cpp
+++ dontstare2/vec2.cpp
@@ -6,3 +6,24 @@ vec2 vec2::operator +(const vec2& b) {
 	a.y += b.y;
 	return a;
 }
+
+vec2 vec2::operator -(const vec2& b) {
+	vec2 a(x, y);
+	a.x -= b.x
+	a.y -= b.y
+	return a;
+}
+
+vec2 vec2::operator *(const vec2& b) {
+	vec2 a(x, y);
+	a.x *= b.x;
+	a.y *= b.y;
+	return a;
+}
+
+vec2 vec2::operator /(const vec2&b) {
+	vec2 a(x, y);
+	a.x /= b.x;
+	a.x /= b.y;
+	return a;
+}
